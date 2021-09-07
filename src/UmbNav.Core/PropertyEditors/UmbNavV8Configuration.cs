f12f2509fd19f3d90ac759907824c40739cd0620
Aaron Sadler <aaron@aaronsadler.uk>
2021-09-07 17:51:52 +0100

    Rearranged configuration items

--- src/UmbNav.Core/PropertyEditors/UmbNavV8Configuration.cs
+++ src/UmbNav.Core/PropertyEditors/UmbNavV8Configuration.cs
@@ -12,16 +12,11 @@ namespace UmbNav.Core.PropertyEditors
         [ConfigurationField("maxDepth", "Max Depth", "number", Description = "The maximum number of levels in the navigation")]
         public int MaxDepth { get; set; }
 
+        [ConfigurationField("hideLabel", "Hide Label", "boolean", Description = "Hide the property label and let the item list span the full width of the editor window.")]
+        public bool HideLabel { get; set; }
+
         [ConfigurationField("removeNaviHideItems", "Remove NaviHide Items", "boolean", Description = "Remove items where umbracoNaviHide is true")]
         public bool RemoveNaviHideItems { get; set; }
-
-        [ConfigurationField("expandOnHover", "Allow Expand On Hover", "boolean",
-            Description = "Expand the tree item on hover")]
-        public bool ExpandOnHover { get; set; } = true;
-
-        [ConfigurationField("expandOnHoverTimeout", "Expand On Hover", "number",
-            Description = "The delay to wait before the tree item expands")]
-        public int ExpandOnHoverTimeout { get; set; } = 500;
 
         [ConfigurationField("hideNoopener", "Hide noopener Toggle", "boolean", Description = "Hide the ability to toggle noopener")]
         public bool HideNoopener { get; set; }
@@ -38,14 +33,18 @@ namespace UmbNav.Core.PropertyEditors
         [ConfigurationField("allowCustomClasses", "Allow Custom Classes", "boolean", Description = "Allow the ability to set custom classes on an item")]
         public bool AllowCustomClasses { get; set; } = false;
 
-        [ConfigurationField("hideLabel", "Hide Label", "boolean", Description = "Hide the property label and let the item list span the full width of the editor window.")]
-        public bool HideLabel { get; set; }
-
         [ConfigurationField("allowImageIcon", "Allow Image / Icon Url", "boolean", Description = "Allow the ability to enter a image / icon url or media id")]
         public bool AllowImageIcon { get; set; }
 
         [ConfigurationField("allowLabels", "Allow Labels", "boolean", Description = "Allow label menu items")]
         public bool AllowLabels { get; set; }
+
+        [ConfigurationField("expandOnHover", "Allow Expand On Hover", "boolean",
+            Description = "Expand the tree item on hover")]
+        public bool ExpandOnHover { get; set; } = true;
+        [ConfigurationField("expandOnHoverTimeout", "Expand On Hover", "number",
+            Description = "The delay to wait before the tree item expands")]
+        public int ExpandOnHoverTimeout { get; set; } = 500;
 
         [ConfigurationField("showTopAddButton", "Show \"Add Item\" Button", "boolean", Description = "Show the \"Add Item\" button above the nav items in the editor")]
         public bool ShowTopAddButton { get; set; } = true;
