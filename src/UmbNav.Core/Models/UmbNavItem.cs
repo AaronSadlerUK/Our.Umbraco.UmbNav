50a1ebc5fd77884576b3713de0b40f0636ac6190
Aaron Sadler <aaron@umbhost.net>
2024-05-30 12:45:37 +0100

    Added a parent property to umbnavitem

--- src/UmbNav.Core/Models/UmbNavItem.cs
+++ src/UmbNav.Core/Models/UmbNavItem.cs
@@ -37,6 +37,9 @@ namespace UmbNav.Core.Models
         public IEnumerable<UmbNavItem> Children { get; set; }
 
         [JsonIgnore]
+        public UmbNavItem Parent { get; set; }
+
+        [JsonIgnore]
         public IPublishedContent Content { get; set; }
 
         [JsonIgnore]
